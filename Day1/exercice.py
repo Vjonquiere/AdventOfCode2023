5b98dd83db9eaf7e0a16db073b78311a916bc0b4
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2023-12-01 14:43:15 +0100

    part2

--- Day1/exercice.py
+++ Day1/exercice.py
@@ -1,7 +1,18 @@
+STR_TO_INT = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+SPELLED_INT = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 def get_input(filePath:str):
     return open(filePath, "r")
 
-def parse(inputFile):
+def find_last_occurence_index(string:str, substring:str):
+    last_index = string.find(substring)
+    for i in range(len(string)):
+        if string.find(substring, i) != -1:
+           last_index = string.find(substring, i)
+    return last_index
+    
+
+def parse(inputFile, secondPart=False):
     total = 0
     lines = inputFile.readlines()
     for line in lines:
@@ -15,10 +26,41 @@ def parse(inputFile):
                     digit[1] = value
             except:
                 pass
+        
+        if secondPart:
+            indexes = [100000, -1]
+            word = ["",""]
+            
+            for number in SPELLED_INT:
+                if number in line:
+                    if line.find(number) != -1 and line.find(number) < line.find(str(digit[0])) and line.find(number) < indexes[0]:
+                        word[0] = number
+                        indexes[0] = line.find(number)
+                        if digit[1] == -1:
+                            digit[1] = digit[0]
+                    if find_last_occurence_index(line, number) != -1 and find_last_occurence_index(line, number) > find_last_occurence_index(line, str(digit[1])) and find_last_occurence_index(line, number) > indexes[1] :
+                        word[1] = number
+                        indexes[1] = find_last_occurence_index(line, number)
+            
+            if word[0] != "":
+                digit[0] = STR_TO_INT[word[0]]
+            if word[1] != "":
+                digit[1] = STR_TO_INT[word[1]]
         if digit[1] == -1:
             digit[1] = digit[0]
+
         total += digit[0]*10 + digit[1]
         
     return total
 
-print(parse(get_input("input")))
+print("Part 1:", parse(get_input("input")))
+print("Part 2:", parse(get_input("input"), True))
+
+
+ ## 55935
+ ## 55901
+ ## 55301
+
+
+ ## 4ninecjzlk7nine
+ ## 5sixtnthdqksseven5
